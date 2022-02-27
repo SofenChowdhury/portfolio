32c94b80f6fef1135a2ae8a0f37b7bdc70b48379
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2022-02-27 16:46:40 +0600

    file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Portfolio from "./Components/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sofen Chowdhury Portfolio
-        </a>
-      </header>
+    <div>
+      <Portfolio/>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Sofen Chowdhury Portfolio
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import './Components/Portfolio'
+import React from 'react';
+
+class Portfolio extends React.Component{
+  render (){
+    return (
+        <div>
+            <h1>Sofen Chowdhury</h1>
+        </div>
+    );
+  }
+}
+
+export default Portfolio;
